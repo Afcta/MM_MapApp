a7f7030a6f183d72056c7cb6c932e667cab4c39e
DaveDave-hub <77210645+DaveDave-hub@users.noreply.github.com>
2023-04-27 17:06:07 +0100

    Revert "Merge branch 'main' of https://github.com/Afcta/MM_MapApp"
    This reverts commit fd350fb428fd9c4d3fd3e21bb0dedf0d1a7262d4, reversing
    changes made to 3704c07b2595753033d0888f4cd51915ec434a4a.

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -1,24 +1,13 @@
 import webbrowser
 import os
 
-from flask import Flask, send_file
-
-app = Flask(__name__)
-
-
-@app.route("Stable.mp4")
-def index():
-    video_path = "./videos/my_video.mp4"
-    mimetype = "video/mp4"
-    return send_file(video_path, mimetype=mimetype)
-
-
 if __name__ == '__main__':
     # Open the menu file and read its contents
-    app.run()
-    os.remove("./videos/my_video.mp4")
     with open('menu.html', 'r') as file:
         menu_html = file.read()
 
     # Open the menu in the default web browser
     webbrowser.open_new_tab('menu.html')
+
+
+
