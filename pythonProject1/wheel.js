72c11e4ca13e309c3bed5ab5aad19cb79b2fd994
DaveDave-hub <77210645+DaveDave-hub@users.noreply.github.com>
2023-05-18 17:28:52 +0100

    updating second world war page

--- pythonProject1/wheel.js
+++ pythonProject1/wheel.js
@@ -1,24 +1,25 @@
 let container = document.querySelector(".container");
 let btn = document.getElementById("spin");
-let number = Math.ceil(Math.random() * 10000);
+let angle = 0;
 
-let flash = document.querySelector(".container div");
+btn.onclick = function () {
+  let randomAngle = Math.ceil(Math.random() * 3600) + 3600; // Generate a random angle between 3600 and 7200 degrees
+  container.style.transform = "rotate(" + randomAngle + "deg)";
 
-
-btn.onclick = function() {
-  container.style.transform = "rotate(" + number + "deg)";
-  number += Math.ceil(Math.random() * 10000);
-
-  setTimeout(function() {
-    let angle = number % 360;
-    if (angle < 90) {
+  setTimeout(function () {
+    angle += randomAngle % 360; // Calculate the new angle by adding the randomAngle modulo 360
+    if (angle < 60) {
+      window.location.href = "map.html";
+    } else if (angle < 120) {
       window.location.href = "worldwar.html";
     } else if (angle < 180) {
-    window.location.href = "https://player.stornaway.io/watch/3e9a9f0e";
-    } else if (angle < 270) {
-    window.location.href = "map.html";
+      window.location.href = "menu.html";
+    } else if (angle < 240) {
+      window.location.href = "https://player.stornaway.io/watch/3e9a9f0e";
+    } else if (angle < 300) {
+      window.location.href = "allVideos.html";
     } else {
-    window.location.href = "allVideos.html";
+      window.location.href = "foodRecipe1.html";
     }
   }, 4000);
 };
